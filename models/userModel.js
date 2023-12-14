ee791a61da10b1851688953156397c38dc6e8651
Nelson Attah <nelson.attah@live.com>
2023-12-14 02:04:12 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,12 @@
 // models/itemModel.js
 
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-// Enhance the user schema with Passport-Local Mongoose functionality
-userSchema.plugin(passportLocalMongoose);
-
-// Create the User model based on the user schema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-
